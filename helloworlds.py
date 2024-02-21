c7d2dd20f54ed8fdf6abe8becc05f7725a76e73c
Cuong Tran <cuongtran73d1@gmail.com>
2024-02-22 06:57:24 +0700

    first commit

---
+++ helloworlds.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author        : Cuong Tran
+
+import os
+import sys
+
+def main():
+    print('Hello worlds from ', os.name)
+
+if __name__ == '__main__':
+    main()
+    # input()
